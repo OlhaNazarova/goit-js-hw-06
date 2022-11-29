6ac710dd7176fece44b160b9b67b10c913bf05ee
OlhaNazarova <108295915+OlhaNazarova@users.noreply.github.com>
2022-11-29 22:27:20 +0200

    task-01

---
+++ src/js/task-01.js
@@ -0,0 +1,9 @@
+const categoriesEl = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesEl.length}`);
+
+categoriesEl.forEach(categorie => {
+    console.log(`Category: ${categorie.firstElementChild.textContent}`);
+    console.log(`Elements: ${categorie.lastElementChild.children.length}`);
+});
+
+
