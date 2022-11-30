b8cb7e99f53b035993e78f811031ffab64b067d6
OlhaNazarova <108295915+OlhaNazarova@users.noreply.github.com>
2022-11-30 19:49:42 +0200

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsListEl = document.querySelector('#ingredients');
+
+const makeIngredientsItem = ingredient => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = ingredient;
+  itemEl.classList.add('item');
+  return itemEl;
+};
+const ingredientsItem = ingredients.map(makeIngredientsItem);
+console.log(ingredientsItem);
+
+ingredientsListEl.append(...ingredientsItem);
